6c77b10c0ee1649867c49519aa4dbb86322cbbef
rob <yes211392@gmail.com>
2020-11-21 12:20:44 +0900

    add multi-thread processing for better performance

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Kumho Compiler
 
 ### The purpose of this compiler is to:
-Convert the DAT files into a CSV file
+Convert the DAT files into a CSV file.
+Save the CSV file in a CSV folder in the parent directory
+
+### Requirement:
+Run the program in a windows machine as it runs subprocess via command line switch.
--- dattocsv.py
+++ dattocsv.py
@@ -1,25 +1,56 @@
 import subprocess
 import re
-from os import listdir
+import os
+import sys
+import time
+import concurrent.futures
 
-# remember to use "/" instead of "\" and remember to put an extra "/" at the end
-file_location = "C:/Users/Robert Choi/Desktop/Kumho ENG/Data/4-7-2018 DATA LOG/BM1 DROP DOOR PT-101 DATA/"
+# time counter to measure program performance
+start_time = time.time()
 
-# extracting a list of target file names
-dirlist = listdir(file_location)
+# set file location
+## remember to use "/" instead of "\" and remember to put an extra "/" at the end
+file_location = "C:/Users/Rob/Documents/Datalog 2018-9-10/BM1(Q MIXER)/PT/BM1 SCRUBBER SC-101-PT-101 DATA/"
+
+#change the current working directory into the parent directory of the file location
+os.chdir(file_location)
+os.chdir("..")
+
+# create a new directory for the csv file using os module
+csv_folder_name = file_location.split("/")
+csv_folder_name = csv_folder_name[len(csv_folder_name) - 2] + "_csv"
+
+# set csv_file_location where new csv files will be stored
+csv_file_location = os.path.abspath(os.curdir) + "/" + csv_folder_name
+csv_file_location = csv_file_location + "/"
+
+try:
+    os.mkdir(csv_folder_name)
+except:
+    sys.exit("There's already a cvs folder created")
+
+# extract a list of target file names
+dirlist = os.listdir(file_location)
 target_file = []
+
 for line in dirlist:
-    #regular expression to find a format "0000 00 00 0014 (Float)"
+    ## regular expression to find a format "0000 00 00 0014 (Float)"
     regex = re.findall(r"[0-9]{4} [0-9]{2} [0-9]{2} [0-9]{2}[1][4] \(Float\)", line)
     if len(regex) == 1:
         target_file.append(regex[0])
 
-# converting dat to csv
-for file in target_file:
+# convert dat to csv using multi-thread processing
+def convert_dat_to_csv(file):
     file_name = file
-    print(file)
-    # using subprocess to run the conversion via command line switch
+    print("converting... ", file)
+    ## using FTViewer's subprocess to run the conversion via command line switch
     viewer_location = file_location + "FTViewFileViewer.exe"
     dat_location = file_location + file_name + ".dat"
-    csv_location = file_location + file_name + ".csv"
+    csv_location = csv_file_location + file_name + ".csv"
     subprocess.run([viewer_location,"/sd", dat_location, csv_location], shell=True)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(convert_dat_to_csv, target_file)
+
+# time counter to measure program performance
+print("Finished in %s seconds" % (round(time.time() - start_time, 3)))
